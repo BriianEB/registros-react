47dbfbd3393e2ede4649f64fb675edc8ea3b0b6a
Brian Becerra <elibecerrahdz@gmail.com>
2023-04-19 17:25:12 -0600

    structure and styles

---
+++ frontend/src/Routes.js
@@ -0,0 +1,16 @@
+import { Routes as RoutesContainer, Route } from 'react-router-dom';
+
+import Layout from './components/shared/Layout';
+import Home from './pages/Home';
+
+function Routes() {
+    return (
+        <RoutesContainer>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+            </Route>
+        </RoutesContainer>
+    );
+}
+
+export default Routes;
